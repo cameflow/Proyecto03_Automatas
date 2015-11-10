98e976df7ceeb7be372f396398e0feacaf9ade1e
Alejandro <alex.terrazas.z@gmail.com>
2015-11-09 23:34:12 -0600

    :rocket: Final implementation

--- automata.cpp
+++ automata.cpp
@@ -1,63 +1,281 @@
-#include "estado.h"
-#include "transicion.h"
 #include <iostream>
+#include <map>
+#include <vector>
 #include <deque>
+#include <stack>
 #include <string>
+#include <utility>
+#include "transicion.h"
 
+//-------------DECLARACIÓN DE VARIABLES--------//
+std::vector<int> x;
+std::vector<std::vector<int> > defVec(11, x);
+std::string expresionRegular;
+std::map<int, std::vector<std::vector<int> > > tabla;
+std::stack<transicion> automatas;
+int next_state = 0;
 
+//-------------DECLARACIÓN DE FUNCIONES--------//
+void polaca();
+int presedencia(char op);
+void con();
+void uni();
+void kleenstar();
+void mas();
+void addTransicion(char a);
+void printTabla();
+void analiza();
+void toNFA();
+
+//-------------MAIN--------//
 int main()
 {
+    expresionRegular = "01*|1";
+    analiza();
+    std::cout << "Regular Expression: " << expresionRegular << std::endl;
+    polaca();
+    std::cout << "Polaca inversa: " << expresionRegular << std::endl;
+    toNFA();
+    printTabla();
+    return 0;
+}
 
-  std::string expresion;
-  int cont = 0;
-  std::deque<char> regEx;
-  std::deque<char> aux;
-  std::vector<transicion> transiciones;
-  std::vector<estado> estados;
+//-------------IMPLEMENTACIÓN DE FUNCIONES--------//
 
-  std::cout << "Ingrese una expresión regular" << std::endl;
-  std::cin >> expresion;
-  std::cout << std::endl;
-
-  //For para llenar la cola con los datos de la expresión regular
-  for (int i = 0; i < expresion.size(); i++)
-  {
-    char caracter = expresion[i];
-
-    //Revisa si todas las entradas fueron validas
-    if (caracter == '0' || caracter == '1' || caracter == '2' || caracter == '3' ||
-      caracter == '4' || caracter == '5' || caracter == '6' || caracter == '7' ||
-      caracter == '8' || caracter == '9' || caracter == '*' || caracter == '+' ||
-      caracter == 'u' || caracter == '(' || caracter == ')' )
+void polaca()
+{
+    std::string input = expresionRegular;
+    std::string output;
+    std::stack<char> operS;
+    char current;
+    while (!input.empty())
     {
-      regEx.push_back(caracter);
+        current = input[0];
+        input.erase(0, 1);
+        switch (current)
+        {
+            case '*': case '+': case '|': case '&':
+                while (!operS.empty() && (presedencia(operS.top()) >= presedencia(current)))
+                {
+                    char holder = operS.top();
+                    operS.pop();
+                    output += holder;
+                }
+                operS.push(current);
+                break;
+
+            case '(':
+                operS.push(current);
+                break;
+
+            case ')':
+                do
+                {
+                    output += operS.top();
+                    operS.pop();
+                } while (operS.top() != '(');
+                operS.pop();
+
+                break;
+
+            default:
+                output += current;
+                break;
+        }
     }
-    else
+    if (!operS.empty())
     {
-        std::cout << "Esa expresión regular no es valida" << std::endl;
-        break;
+        while (!operS.empty())
+        {
+            if (!(operS.top() == '(' || operS.top() == ')'))
+            {
+                output += operS.top();
+                operS.pop();
+            }
+        }
     }
-  }
 
-  while(!regEx.empty())
-  {
-    aux.push_back(regEx.front());
-    estado a;
-    estado b;
-    a.setID(cont);
-    cont++;
-    b.setID(cont);
-    cont++;
-    estados.push_back(a);
-    estados.push_back(b);
-    transicion t = transicion(a.getID(),b.getID(),regEx.front()-48);
-    transiciones.push_back(t);
-    regEx.pop_front();
-  }
+    expresionRegular = output;
+}
 
+int presedencia(char op)
+{
+    switch (op)
+    {
+        case '*':
+            return 4;
+        case '&':
+            return 3;
+        case '+':
+            return 2;
+        case '|':
+            return 1;
+        default:
+            return 0;
+    }
+}
 
+void con()
+{
+    transicion aux2 = automatas.top();
+    automatas.pop();
+    transicion aux1 = automatas.top();
+    automatas.pop();
+    tabla.find(aux1.final)->second[10].push_back(aux2.origen);
+    transicion b;
+    b.origen = aux1.origen;
+    b.final = aux2.final;
+    automatas.push(b);
+}
 
+void uni()
+{
+    transicion aux2 = automatas.top();
+    automatas.pop();
+    transicion aux1 = automatas.top();
+    automatas.pop();
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.find(next_state-2)->second[10].push_back(aux1.origen);
+    tabla.find(next_state-2)->second[10].push_back(aux2.origen);
+    tabla.find(aux1.final)->second[10].push_back(next_state-1);
+    tabla.find(aux2.final)->second[10].push_back(next_state-1);
+    transicion auxX;
+    auxX.origen = next_state-2;
+    auxX.final = next_state -1;
+    automatas.push(auxX);
+}
 
+void kleenstar()
+{
+    transicion aux = automatas.top();
+    automatas.pop();
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.find(aux.final)->second[10].push_back(aux.origen);
+    tabla.find(next_state-2)->second[10].push_back(aux.origen);
+    tabla.find(next_state-2)->second[10].push_back(next_state-1);
+    tabla.find(aux.final)->second[10].push_back(next_state-1);
+    transicion auxX;
+    auxX.origen = next_state-2;
+    auxX.final = next_state -1;
+    automatas.push(auxX);
+}
 
-  return 0;
+void mas()
+{
+    transicion aux = automatas.top();
+    automatas.pop();
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.find(aux.final)->second[10].push_back(aux.origen);
+    tabla.find(next_state-2)->second[10].push_back(aux.origen);
+    tabla.find(aux.final)->second[10].push_back(next_state-1);
+    transicion auxX;
+    auxX.origen = next_state-2;
+    auxX.final = next_state -1;
+    automatas.push(auxX);
+}
+
+void addTransicion(char a)
+{
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.insert(std::pair<int, std::vector<std::vector<int> > >(next_state++ , defVec));
+    tabla.find(next_state-2)->second[a-'0'].push_back(next_state-1);
+    transicion aux;
+    aux.origen = next_state-2;
+    aux.final = next_state -1;
+    automatas.push(aux);
+}
+
+void printTabla()
+{
+    std::map<int, std::vector<std::vector<int> > >::iterator it;
+    for (it=tabla.begin(); it != tabla.end(); ++it)
+    {
+        std::cout << "q" << it->first << " => ";
+        int k = 0;
+        for (auto i : it->second)
+        {
+
+          std::cout << "-";
+          std::cout << "|";
+          for (auto j : i)
+          {
+              std::cout << j << ",";
+          }
+          std::cout << "|";
+
+        }
+        std::cout << std::endl;
+    }
+}
+
+void analiza()
+{
+    unsigned long int tam = expresionRegular.size();
+    for (int i = 0; i < tam; i++)
+    {
+        if (expresionRegular[i] >= '0' && expresionRegular[i] <= '9')
+        {
+            if (expresionRegular[i+1] >= '0' && expresionRegular[i+1] <= '9')
+            {
+                expresionRegular.insert(i+1, "&");
+                tam++;
+                i++;
+            }
+            else if (expresionRegular[i+1] == '(')
+            {
+                expresionRegular.insert(i+1, "&");
+                tam++;
+                i++;
+            }
+        }
+        else if (expresionRegular[i] == '*' || expresionRegular[i] == '+')
+        {
+            if (expresionRegular[i+1] >= '0' && expresionRegular[i+1] <= '9')
+            {
+                expresionRegular.insert(i+1, "&");
+                tam++;
+                i++;
+            }
+            else if (expresionRegular[i+1] == '(')
+            {
+                expresionRegular.insert(i+1, "&");
+                tam++;
+                i++;
+            }
+        }
+        else if (expresionRegular[i] == ')' && expresionRegular[i+1] == '(')
+        {
+            expresionRegular.insert(i+1, "&");
+            tam++;
+            i++;
+        }
+    }
+}
+
+void toNFA()
+{
+    for (int i = 0; i < expresionRegular.size(); i++)
+    {
+        switch (expresionRegular[i])
+        {
+            case '&':
+                con();
+                break;
+            case '|':
+                uni();
+                break;
+            case '*':
+                kleenstar();
+                break;
+            case '+':
+                mas();
+                break;
+            default:
+                addTransicion(expresionRegular[i]);
+                break;
+        }
+    }
 }
--- transicion.h
+++ transicion.h
@@ -2,34 +2,6 @@ class transicion
 {
 public:
   int origen;
-  int destino;
-  int valor;
+  int final;
 
-  transicion();
-  transicion(int o, int d, int v);
-  int getOrigen();
-  int getDestino();
-  int getValor();
 };
-
-transicion::transicion(int o, int d, int v)
-{
-  origen = o;
-  destino = d;
-  valor = v;
-}
-
-int transicion::getOrigen()
-{
-  return origen;
-}
-
-int transicion::getDestino()
-{
-  return destino;
-}
-
-int transicion::getValor()
-{
-  return valor;
-}
